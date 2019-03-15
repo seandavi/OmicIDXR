e9ddf359e144533245e148cb4b97cd8c344c0d39
Sean Davis <seandavi@gmail.com>
2019-03-15 17:23:36 -0400

    add examples to query

--- R/query.R
+++ R/query.R
@@ -3,6 +3,36 @@
 #'
 #' @importFrom ghql Query
 #'
+#'
+#' @examples
+#' # create a client
+#' cli = OmicIDXGraphqlClient$new()
+#'
+#' # create a query container
+#' qc = OmicIDXQuery$new()
+#'
+#' qc$query('allSraStudies', '
+#' { allSraStudies
+#'   {
+#'     edges {
+#'       node {
+#'         accession
+#'         bioproject
+#'         gse
+#'         title
+#'         status
+#'         updated
+#'       }
+#'     }
+#'   }
+#' }
+#' ')
+#'
+#' results = dataframe_handler(cli$exec(qc$queries$allSraStudies))
+#' dim(results)
+#' head(results)
+#'
+#'
 #' @export
 OmicIDXQuery <- R6::R6Class(
   "OmicIDXQuery",
